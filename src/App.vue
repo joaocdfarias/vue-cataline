5f78c13ff064e33fe5300eea3daf87e8e7125420
João Carlos <joaocdfariass@gmail.com>
2022-02-20 20:57:40 -0300

    classes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,25 @@
 <template>
-  <h1 v-for="todo in todosFilter" :key="todo.text">{{ todo.text }}</h1>
-  <button @click="checkAllTodos">Finalizar</button>
+  <h1 :class="['static', { 'text-danger': hasError }]">Cataline</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-interface Todos {
-  text: string
-  done: boolean
-}
-
 export default defineComponent({
   data() {
     return {
-      todos: [] as Todos[]
-    }
-  },
-  computed: {
-    todosFilter(): Todos[] {
-      return this.todos.filter((todo) => todo.done)
-    }
-  },
-  created() {
-    this.todos = [
-      { text: 'Estudar Typescript', done: true },
-      { text: 'Lavar os pratos', done: false },
-      { text: 'Aprender Nuxt.js', done: true }
-    ]
-  },
-  watch: {
-    todos(newValue: Todos[]) {
-      const isFinished = !newValue.some(({ done }) => !done)
-
-      if (isFinished) {
-        alert('Todos finalizados')
-      }
-    }
-  },
-  methods: {
-    checkAllTodos() {
-      this.todos = this.todos.map(({ text }) => {
-        return { text, done: true }
-      })
+      isActive: true,
+      hasError: false
     }
   }
 })
 </script>
+
+<style scoped>
+.test {
+  color: red;
+}
+.active {
+  color: green;
+}
+</style>
